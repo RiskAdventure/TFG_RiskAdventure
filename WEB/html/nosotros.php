c8c4b82e3cab29970306b6b48cfe09c5409d793e
Pilar Bermejo <pbermejodepedro@gmail.com>
2021-04-10 20:28:01 +0200

    Cambio nombre pestañas navegador de todas las landing

--- WEB/html/nosotros.php
+++ WEB/html/nosotros.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Bootstrap Example</title>
+    <title>Nosotros</title>
     <meta charset="utf-8">
     <meta name="keywords" content="">
     <meta name="description" content="">
