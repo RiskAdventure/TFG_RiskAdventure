b548b6dbdefe6150831e1c0e59f323432107969a
Pilar Bermejo <pbermejodepedro@gmail.com>
2021-04-11 18:17:53 +0200

    Quito la s de home.css que estaba en plural

--- WEB/html/experiencias.php
+++ WEB/html/experiencias.php
@@ -31,52 +31,7 @@
         </ul>
       </header>
       <section>
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-          </ol>
-
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner">
-
-            <div class="item active">
-              <img src="../imagenes/aircraft-1499171__480.webp" alt="Los Angeles" >
-              <div class="carousel-caption">
-                <h3>EXPERIENCIAS ÚNICAS</h3>
-                <p>Sin riesgo no hay aventura, sumérgete en una nueva sensación...</p>
-              </div>
-            </div>
-
-            <div class="item">
-              <img src="../imagenes/mountains-276995__480.jpg" alt="Chicago" >
-              <div class="carousel-caption">
-              <h3>EXPERIENCIAS ÚNICAS</h3>
-                <p>Sin riesgo no hay aventura, sumérgete en una nueva sensación...</p>
-              </div>
-            </div>
-          
-            <div class="item">
-              <img src="../imagenes/man-384178__480.webp" alt="New York" >
-              <div class="carousel-caption">
-              <h3>EXPERIENCIAS ÚNICAS</h3>
-                <p>Sin riesgo no hay aventura, sumérgete en una nueva sensación...</p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
+        
     
         <!-- Texto bajo carrusel -->
         <div id="texto">
