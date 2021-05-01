ee2aeb80d249492e82c15cb2fd84c2a3466c9153
Pilar Bermejo <pbermejodepedro@gmail.com>
2021-05-01 17:56:51 +0200

    añado botón al blog al final de los artículos

--- WEB/html/blog.php
+++ WEB/html/blog.php
@@ -105,7 +105,7 @@
              <!-- botón central inferior blog-->
              <section class="row" >
                 <div class="col-md-12 col-xs-12 col-xm-12 " id="boton_blog" > 
-                <a href="blog.php"><div  class="boton_blog_centro"> IR AL BLOG</div></a>
+                <a href="blog.php"><div  class="boton_blog_centro"> VER MÁS ARTÍCULOS</div></a>
                 </div>
             </section>
         
