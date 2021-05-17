8cbe5ed39a7550d0064aea5e27bd31e812a5ed9e
Pilar Bermejo <pbermejodepedro@gmail.com>
2021-05-17 12:28:27 +0200

    actualizo

--- WEB/blog/wp-config.php
+++ WEB/blog/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'RISK' );
+define( 'DB_NAME', 'ddb168997' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'ddb168997' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'Risk*9999' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'bbdd.riskadventureclub.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
