89cce58407917530c2ee192a7e845b4bfc91c8a8
Pilar Bermejo <pbermejodepedro@gmail.com>
2021-05-20 16:50:53 +0200

    añado cdn en todas las landing para que salga bien en hosting

--- WEB/html/contacto.php
+++ WEB/html/contacto.php
@@ -12,6 +12,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><!-- cdn para librería jquery-->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script><!--cdn para framework bootstrap-->
     <link rel="stylesheet" href="../css/contacto.css" type="text/css"> <!--Enlace externo a CSS-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
     <!--Cargado desde un archivo JQUERY descargado en nuestro proyecto-->
     <script type="text/javascript" src="JS/jquery-3.5.1.min.js"></script>
     <link rel="icon" type="image/png" href="../imagenes/favicon_risk2.png" sizes="64x64"><!--Favicon RISK-->
--- WEB/html/login.php
+++ WEB/html/login.php
@@ -18,6 +18,7 @@
 
 	<!--Custom styles-->
 	<link rel="stylesheet" type="text/css" href="../css/login.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
